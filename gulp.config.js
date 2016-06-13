05126c0f9f4446de8c745ee948906c27a08d1c7d
gthio <gunawanthio@outlook.com>
2016-06-13 18:10:17 +0800

    add gulp task to clean build folder

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,11 @@
 module.exports = function(){
+
+  var root = './';
+  var build = root + '.build/';
   
   var gulpConfig = {
+      
+    build: build,
   };  
   
   return gulpConfig;
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
 var gulp = require('gulp');
 var util = require('gulp-util');
+var del = require('del');
+var gulpConfig = require('./gulp.config')();
+
+var $ = require('gulp-load-plugins')({lazy: true});
+
+gulp.task('clean-build', function(done){
+  clean(gulpConfig.build, done);
+});
 
 gulp.task('test', function(done){
   log('test');
 });
 
+function clean(path, done){
+  log('Cleaning: ' + $.util.colors.blue(path));
+  del(path).then(done());
+}
+
 function log(msg){
-	if (typeof(msg) === 'object'){
-		for (var item in msg){
-			if (msg.hasOwnProperty(item)){
-				util.log(util.colors.blue(msg[item]));
-			}
-		}
-	}	
-	else {
-		util.log(util.colors.blue(msg));
+
+  if (typeof(msg) === 'object'){
+    for (var item in msg){
+	  if (msg.hasOwnProperty(item)){
+	    util.log(util.colors.blue(msg[item]));
+	  }
 	}
+  }	
+  else {
+    util.log(util.colors.blue(msg));
+  }
 }
