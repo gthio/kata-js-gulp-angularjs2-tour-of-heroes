b5ef794e4c282fab4c2b96f92fbaec7f3fa2afa6
gthio <gunawanthio@outlook.com>
2016-06-13 17:51:05 +0800

    implement log function

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,19 @@
 var gulp = require('gulp');
+var util = require('gulp-util');
 
+gulp.task('test', function(done){
+  log('test');
+});
+
+function log(msg){
+	if (typeof(msg) === 'object'){
+		for (var item in msg){
+			if (msg.hasOwnProperty(item)){
+				util.log(util.colors.blue(msg[item]));
+			}
+		}
+	}	
+	else {
+		util.log(util.colors.blue(msg));
+	}
+}
